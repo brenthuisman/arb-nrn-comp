a7fb2e36baec153dfeba0e2734005a99d56d5dd1
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-02 19:05:37 +0200

    switched new to old

--- comp_ovn.py
+++ comp_ovn.py
@@ -4,24 +4,24 @@ from patch import p
 import plotly.graph_objs as go
 import time
 
-def run_nrn(setup, model, model_new, duration, v_init):
+def run_nrn(setup, model, model_old, duration, v_init):
     if setup is not None:
         pass
     neuron_cell = model()
     neuron_cell.record_soma()
-    neuron_cell_new = model_new()
-    neuron_cell_new.record_soma()
+    neuron_cell_old = model_old()
+    neuron_cell_old.record_soma()
     neuron_time = p.time
     p.dt = 0.025
     p.cvode.active(0)
     p.celsius = 32
     p.finitialize(v_init)
     p.continuerun(duration)
-    return (neuron_time, neuron_cell.Vm), (neuron_time, neuron_cell_new.Vm)
+    return (neuron_time, neuron_cell.Vm), (neuron_time, neuron_cell_old.Vm)
 
 if __name__ == "__main__":
     setups = {
-        v: {"model": getattr(models, v), "new_model": getattr(models, v + "New")}
+        v: {"model": getattr(models, v), "model_old": getattr(models, v + "Old")}
         for v in sys.argv
         if v.endswith("Cell")
     } or {
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     for name, setup in setups.items():
         print(setup)
         model = setup["model"]
-        model_new = setup["new_model"]
+        model_old = setup["model_old"]
         print("Running", name)
-        (nt, nv), (ot, ov) = run_nrn(None, model, model_new, 1000, -65)
+        (nt, nv), (ot, ov) = run_nrn(None, model, model_old, 1000, -65)
         print("Setup", name, "finished")
         go.Figure(
             [
@@ -46,7 +46,7 @@ if __name__ == "__main__":
                 go.Scatter(
                     x=ot,
                     y=ov,
-                    name=f"{name} - NEW"
+                    name=f"{name} - OLD"
                 ),
             ]
         ).show()
