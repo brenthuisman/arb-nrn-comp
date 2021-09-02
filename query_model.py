1e021679433f7284862eb2308475dd6d66361924
Robin De Schepper <robin.deschepper93@gmail.com>
2021-09-02 15:52:48 +0200

    add extra scripts

---
+++ query_model.py
@@ -0,0 +1,22 @@
+import sys
+import dbbs_models
+
+model = getattr(dbbs_models, sys.argv[1])
+cell = model()
+
+type_alias = {
+    "s": "soma",
+    "d": "dendrites",
+    "a": "axon",
+    "aa": "ascending_axon",
+    "pf": "parallel_fiber"
+}
+
+queries = sys.argv[1:]
+for query in queries:
+    types, asks = query.split(":")
+    sections = list(s for s in cell.sections if any(t in s.labels for t in types))
+    if "L" in asks:
+        print("Length of", types, len(sections))
+    if "M" in asks:
+        print("Mechanism dict of",)
---
+++ restore_golgi.py
@@ -0,0 +1,6 @@
+from dbbs_models import GolgiCell
+from dbbs_models.golgi_cell_models import OldGolgi
+from dbbs_models.test import quick_test
+
+g_new = GolgiCell()
+g_old = OldGolgi()
