96c01719380f6f27b4457a5604aa5a6d8583c90d
Robin De Schepper <robin.deschepper93@gmail.com>
2021-12-15 16:15:27 +0100

    cosi

--- plots/mechanisms.py
+++ plots/mechanisms.py
@@ -1,6 +1,7 @@
 import plotly.graph_objs as go
 from pathlib import Path
 import numpy as np
+import math
 
 def plot():
     bigger_path = Path(__file__).parent.parent / "results" / "bigger_test"
@@ -11,7 +12,7 @@ def plot():
     m = np.mean(matrix, axis=1) / m
     return go.Figure(go.Histogram(x=m, marker_line_width=0), layout = dict(
         xaxis_title="Mechanism combination speedup",
-        xaxis_range=[0, 20],
+        xaxis_type="log",
         yaxis_title="Count",
         bargap=0.0,
         bargroupgap=0.0,
