46440f0eff81d7e84de78924d49597d318c3b460
Robin De Schepper <robin.deschepper93@gmail.com>
2022-04-15 08:33:59 +0200

    brushed up single cell fig

--- plots/single_cell_isi.py
+++ plots/single_cell_isi.py
@@ -26,15 +26,20 @@ def plot(run_locally=False):
         cols=2,
         subplot_titles=("Stellate cell", "Basket cell", "Golgi cell", "Purkinje cell"),
         x_title="Spike pair",
-        y_title="Interspike interval (ms)"
+        y_title="Interspike interval (ms)",
+        specs=[
+            [{"l":0.03, "t": 0.03}, {"t": 0.03}],
+            [{"l":0.03, "b": 0.03}, {"b": 0.03}],
+        ]
     )
     fig.update_layout(title="Single cell ISI")
+    fig.update_annotations(font_size=35)
     ctr = 0
     for name, (nrn_t, nrn_v), (arb_t, arb_v) in ((name, nrn_data[name], arb_data[name]) for name in arb_data.keys()):
         if name == "GranuleCell":
             continue
         data = []
-        for l, s, dt in (("arbor", arb_v, 0.1), ("neuron", nrn_v, 0.025)):
+        for l, s, dt, c in (("arbor", arb_v, 0.1, "255,127,14"), ("neuron", nrn_v, 0.025, "31,119,180")):
             s = np.array(s)
             p, props = find_peaks(s, height=-10)
             isi = np.diff(p) * dt
@@ -44,6 +49,10 @@ def plot(run_locally=False):
                     y=isi,
                     name=l,
                     mode="lines+markers",
+                    line=dict(color=f'rgb({c})'),
+                    marker=dict(color=f'rgb({c})'),
+                    legendgroup=l,
+                    showlegend=not ctr
                 ),
             )
         fig.add_traces(data, rows=(ctr // 2) + 1, cols=(ctr % 2) + 1)
--- plots/single_cell_vm.py
+++ plots/single_cell_vm.py
@@ -21,27 +21,41 @@ def plot(run_locally=False):
         cols=2,
         subplot_titles=("Stellate cell", "Basket cell", "Golgi cell", "Purkinje cell"),
         y_title="Vm (mV)",
-        x_title="Time (ms)"
+        x_title="Time (ms)",
+        specs=[
+            [{"l":0.03, "t": 0.03}, {"t": 0.03}],
+            [{"l":0.03, "b": 0.03}, {"b": 0.03}],
+        ]
     )
     fig.update_layout(title="Single cell Vm")
+    fig.update_annotations(font_size=35)
     ctr = 0
     for name in arb_data.keys():
+        if name == "GranuleCell":
+            continue
         fig.add_traces(
             [
-                go.Scatter(
-                    x=(data := arb_data[name])[0],
-                    y=data[1],
-                    name="Arbor",
-                ),
                 go.Scatter(
                     x=(data := nrn_data[name])[0],
                     y=data[1],
                     name="NEURON",
+                    line=dict(color=f'rgb(31,119,180)', width=1),
+                    marker=dict(color=f'rgb(31,119,180)'),
+                    legendgroup="n",
+                    showlegend=not ctr
+                ),
+                go.Scatter(
+                    x=(data := arb_data[name])[0],
+                    y=data[1],
+                    name="Arbor",
+                    line=dict(color=f'rgb(255,127,14)', width=1),
+                    marker=dict(color=f'rgb(255,127,14)'),
+                    legendgroup="a",
+                    showlegend=not ctr
                 ),
             ],
             rows=(ctr // 2) + 1, cols=(ctr % 2) + 1
         )
-        if name != "GranuleCell":
-            ctr += 1
+        ctr += 1
 
     return fig
