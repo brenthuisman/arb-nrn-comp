cdb781ca5014f355f0d0119d6563b25ad243eb14
Robin De Schepper <robin.deschepper93@gmail.com>
2022-05-16 11:47:08 +0200

    Added conditional neuron/coreneuron/gpu jobs

--- templates/neuron.sh
+++ templates/neuron.sh
@@ -12,7 +12,18 @@
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export HDF5_USE_FILE_LOCKING=FALSE
-source $HOME/nrnenv/bin/activate
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/mpt/7.7.18/gni/mpich-crayclang/10.0/lib
+
+## if: coreneuron and not gpu
+## source $HOME/corenrn/bin/activate
+## nrnivmodl -coreneuron $HOME/corenrn/dbbs-mod-collection/dbbs_mod_collection/mod
+## if: coreneuron and gpu
+## source $HOME/corenrn-gpu/bin/activate
+## nrnivmodl -coreneuron -gpu $HOME/corenrn/dbbs-mod-collection/dbbs_mod_collection/mod
+## if: coreneuron
+## export GLIA_NOCOMPILE=TRUE
+## export GLIA_NOLOAD=TRUE
+## if: not coreneuron
+## source $HOME/nrnenv/bin/activate
 
 srun bsb -v 4 simulate @@name@@ --hdf5=$HOME/arb-nrn-benchmarks-rdsea-2022/models/@@name@@.hdf5
