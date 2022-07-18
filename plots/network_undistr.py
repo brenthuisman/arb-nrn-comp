cfd84d260931adafa09c78461cfb50c6ae1c592d
Robin De Schepper <robin.deschepper93@gmail.com>
2022-07-18 19:23:40 +0200

    did some renaming. merged sock and single

--- plots/network_undistr.py
+++ plots/network_undistr.py
@@ -26,22 +26,26 @@ def plot():
             "nh": "std",
         }
     )
-    benches.loc[1:5, "bench_name"] = (
-        "NEURON",
-        "Arb. single",
-        "Arb. multithr.",
-        "Arb. hyperthr.",
-        "Arb. GPU",
+    col_ids = [1, 14, 19, 2, 13, 3, 4, 5]
+    benches.loc[col_ids, "bench_name"] = (
+        "NEURON 1-thread",
+        "NEURON 18-thread",
+        "NEURON 36-thread",
+        "Arbor 1-thread",
+        "Arbor 18-thread",
+        "Arbor 36-thread",
+        "Arbor 72-thread",
+        "Arbor GPU",
     )
     return {
         cat: go.Figure(
             data=[
                 go.Scatter(
-                    x=benches["bench_name"].loc[1:5],
-                    y=benches[cat].loc[1:5],
+                    x=benches["bench_name"].loc[col_ids],
+                    y=benches[cat].loc[col_ids],
                     error_y=dict(
                         type="data",
-                        array=benches_err[cat].loc[1:5],
+                        array=benches_err[cat].loc[col_ids],
                         thickness=5,
                         width=15,
                         # color=[ #terrible plotly does not let you set colors per datapoints
@@ -56,6 +60,9 @@ def plot():
                     marker = dict(size = 15),
                     marker_color=[
                         "rgb(31, 119, 180)",
+                        "rgb(31, 119, 180)",
+                        "rgb(31, 119, 180)",
+                        "rgb(255,127,14)",
                         "rgb(255,127,14)",
                         "rgb(255,127,14)",
                         "rgb(255,127,14)",
