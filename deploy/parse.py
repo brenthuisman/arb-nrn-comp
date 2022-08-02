ccc480e4f619a5c63753aecd73718d874a5ab7bb
Robin De Schepper <robin.deschepper93@gmail.com>
2022-08-02 13:04:14 +0200

    copy fix

--- deploy/parse.py
+++ deploy/parse.py
@@ -10,7 +10,7 @@ ids = pd.read_csv(args[1])
 if len(args) > 2:
     log_dir = pathlib.Path(args[2])
 else:
-    log_dir = ids_path.copy()
+    log_dir = ids_path
 home = pathlib.Path(os.getenv("HOME"))
 batch_dir = home / "results" / name
 batch_dir.mkdir(parents=True, exist_ok=False)
