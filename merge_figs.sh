4a5241c786f6fed9421aada39d89b89b42286ca6
Robin De Schepper <robin.deschepper93@gmail.com>
2022-08-02 12:51:06 +0200

    updated merge figs

--- merge_figs.sh
+++ merge_figs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mkdir -p figures_paper
-montage figures/mechanisms.jpg figures/mech_features_kde_max.jpg -tile 2x1 -geometry +0+0 figures_paper/mech.jpg
-montage figures/network_single_tts.jpg figures/network_single_spms.jpg figures/network_single_e.jpg -tile 3x1 -geometry +0+0 figures_paper/network_single.jpg
-montage figures/network_sock_tts.jpg figures/network_sock_spms.jpg figures/network_sock_e.jpg -tile 3x1 -geometry +0+0 figures_paper/network_sock.jpg
-montage figures/network_distr_tts.jpg figures/network_distr_spms.jpg figures/network_distr_e.jpg -tile 3x1 -geometry +0+0 figures_paper/network_distr.jpg
+
+montage figures/single_cell_vm.png figures/single_cell_isi.png -tile 1x2 -geometry +0+0 figures_paper/validation.png
+montage figures/mechanisms.png figures/mech_features_kde_max.png figures/single_cell.png -tile 3x1 -geometry +0+0 figures_paper/perfcomp.png
+
+montage figures/network_distr_tts.png figures/network_distr_spms.png figures/network_distr_e.png -tile 3x1 -geometry +0+0 figures_paper/network_distr.png
+montage figures/network_undistr_tts.png figures/network_undistr_spms.png figures/network_undistr_e.png -tile 3x1 -geometry +0+0 figures_paper/network_undistr.png
 montage figures/network_gpu_tts.jpg figures/network_gpu_spms.jpg figures/network_gpu_e.jpg -tile 3x1 -geometry +0+0 figures_paper/network_gpu.jpg
-montage figures/single_cell.jpg figures/single_cell_isi.jpg figures/single_cell_vm.jpg -tile 3x1 -geometry +0+0 figures_paper/single_cell.jpg
